a71546cbd0ea50b4447ae8bc6cf462e1a5e04186
wingertge <wingertbe@gmail.com>
2020-01-05 12:52:41 +0000

    Deploying to gh-pages from master 727b8dec55a471615aa558edbb5652cf78d428c6

--- implementors/failure/trait.Fail.js
+++ implementors/failure/trait.Fail.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["backend_rust"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"backend_rust/auth/enum.JWTError.html\" title=\"enum backend_rust::auth::JWTError\">JWTError</a>",synthetic:false,types:["backend_rust::auth::JWTError"]},];
 implementors["cookie_store"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"cookie_store/struct.IdnaErrors.html\" title=\"struct cookie_store::IdnaErrors\">IdnaErrors</a>",synthetic:false,types:["cookie_store::IdnaErrors"]},];
 implementors["failure"] = [];
 
